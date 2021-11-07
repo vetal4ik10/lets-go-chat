a47ff33cbeb2be5af01baf63c2a07d304b0872a8
Vitalii Kotlyarets <v.kotlyarets@dev-branch.com>
2021-11-07 21:42:15 +0200

    Added ENV port

--- cmd/lets-go-chat/main.go
+++ cmd/lets-go-chat/main.go
@@ -4,11 +4,19 @@ import (
 	"github.com/vetal4ik10/lets-go-chat/internal/endpoints"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+
 	http.HandleFunc("/user", endpoints.UserCreate)
 	http.HandleFunc("/user/list", endpoints.UserList)
 	http.HandleFunc("/user/login", endpoints.Login)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":" + port, nil))
 }
