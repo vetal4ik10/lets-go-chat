62462579a3240b0ead5b2cef54ab27be8c223b91
Vitalii Kotlyarets <v.kotlyarets@dev-branch.com>
2021-11-23 22:30:01 +0200

    Update handlers

--- cmd/lets-go-chat/main.go
+++ cmd/lets-go-chat/main.go
@@ -1,19 +1,50 @@
 package main
 
 import (
+	"database/sql"
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 	"github.com/vetal4ik10/lets-go-chat/configs"
 	"github.com/vetal4ik10/lets-go-chat/internal/handlers"
+	"github.com/vetal4ik10/lets-go-chat/internal/reposetories"
 	"log"
 	"net/http"
 )
 
+func initDatabase() (*sql.DB) {
+	// Init database.
+	dataSoutceName := configs.GetPostgresUrl()
+	db, err := sql.Open("postgres", dataSoutceName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	schema(db)
+
+	return db
+}
+
+func schema(db *sql.DB) {
+	drop := "DROP TABLE IF exists users;"
+	create := drop + "CREATE TABLE users (" +
+		"uid varchar(60) NOT NULL, " +
+		"name varchar(60) NOT NULL, " +
+		"pass varchar(255) DEFAULT NULL" +
+		")"
+	_, err := db.Query(create)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
-	//.Headers("Content-Type", "application/json")
+	db := initDatabase()
+	userRepo := reposetories.NewUserRepo(db)
+	userH := handlers.NewUserHandlers(userRepo)
 
 	r := mux.NewRouter()
-	r.HandleFunc("/user", handlers.UserCreate).Methods("POST")
-	r.HandleFunc("/user/login", handlers.Login).Methods("POST")
-	r.HandleFunc("/user/list", handlers.UserList).Methods("GET")
+	r.HandleFunc("/user", userH.UserCreate).Methods("POST")
+	r.HandleFunc("/user/login", userH.Login).Methods("POST")
+	r.HandleFunc("/user/list", userH.UserList).Methods("GET")
 	log.Fatal(http.ListenAndServe(":"+configs.GetServerPort(), r))
 }
--- internal/handlers/users.go
+++ internal/handlers/users.go
@@ -4,29 +4,37 @@ import (
 	"encoding/json"
 	"errors"
 	"github.com/vetal4ik10/hasher"
-	"github.com/vetal4ik10/lets-go-chat/internal/env"
 	"github.com/vetal4ik10/lets-go-chat/internal/models"
+	"github.com/vetal4ik10/lets-go-chat/internal/reposetories"
 	"net/http"
 )
 
-type loginRequest struct {
-	UserName string
-	Password string
+type UserHandlers struct {
+	repo reposetories.UserRepo
 }
 
-func validateUser(user *models.User) error {
+func NewUserHandlers(repo reposetories.UserRepo) *UserHandlers {
+	return &UserHandlers{repo}
+}
+
+func (uH *UserHandlers) validateUser(user *createUserRequest) error {
 	if len(user.UserName) < 4 {
 		return errors.New("user name should contain at least 4 chars")
 	} else if len(user.Password) < 8 {
 		return errors.New("user password should contain at least 8 chars")
-	} else if u, _ := env.GetUserRepo().GetByUserName(user.UserName); u != nil {
+	} else if u, _ := uH.repo.GetByUserName(user.UserName); u != nil {
 		return errors.New("user is already exist")
 	}
 	return nil
 }
 
-func UserCreate(w http.ResponseWriter, r *http.Request) {
-	var userRequest models.User
+type createUserRequest struct {
+	UserName string `json:"userName"`
+	Password string	`json:"password"`
+}
+
+func (uH *UserHandlers) UserCreate(w http.ResponseWriter, r *http.Request) {
+	var userRequest createUserRequest
 
 	// Parse post body.
 	err := json.NewDecoder(r.Body).Decode(&userRequest)
@@ -36,20 +44,24 @@ func UserCreate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Validate new user.
-	err = validateUser(&userRequest)
+	err = uH.validateUser(&userRequest)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	userRequest.Password, _ = hasher.HashPassword(userRequest.Password)
-	err = env.GetUserRepo().Create(&userRequest)
+	// Create new user object.
+	user := &models.User{
+		UserName: userRequest.UserName,
+		Password: userRequest.Password,
+	}
 
+	err = uH.repo.Create(user);
 	// Create new user.
 	if err == nil {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusCreated)
-		u, _ := env.GetUserRepo().GetByUserName(userRequest.UserName)
+		u, _ := uH.repo.GetByUserName(userRequest.UserName)
 		response := map[string]string{"id": u.Uid, "userName": u.UserName}
 		json.NewEncoder(w).Encode(response)
 	} else {
@@ -57,7 +69,13 @@ func UserCreate(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func Login(w http.ResponseWriter, r *http.Request) {
+
+type loginRequest struct {
+	UserName string `json:"userName"`
+	Password string	`json:"password"`
+}
+
+func (uH *UserHandlers) Login(w http.ResponseWriter, r *http.Request) {
 	var loginRequest loginRequest
 
 	// Parse post body.
@@ -67,7 +85,7 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	user, err := env.GetUserRepo().GetByUserName(loginRequest.UserName)
+	user, err := uH.repo.GetByUserName(loginRequest.UserName)
 	if err != nil || !hasher.CheckPasswordHash(loginRequest.Password, user.Password) {
 		http.Error(w, "User name or password is incorrect.", http.StatusBadRequest)
 		return
@@ -81,11 +99,9 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
-func UserList(w http.ResponseWriter, r *http.Request) {
-	repo := env.GetUserRepo()
-	repo.Create(&models.User{UserName: "test", Password: "test"})
-
-	user, _ := repo.GetByUserName("test")
+func (uH *UserHandlers) UserList(w http.ResponseWriter, r *http.Request) {
+	uH.repo.Create(&models.User{UserName: "test", Password: "test"})
+	user, _ := uH.repo.GetByUserName("test")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(user)
 }
